5b2343c57e6152d414e8800139908e998910d060
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-01 18:12:11 +0200

    Created the function

--- support/TestDataLoader.py
+++ support/TestDataLoader.py
@@ -1,7 +1,14 @@
 import json
+from support.FileNames import FileNames
+from support.JSONKeys import JSONKeys
 
 
 def load_test_data(file_name, key):
-    f = open(f'../testdata/{file_name}')
-    data = json.load(f)
+    with open(f'../testdata/{file_name}', 'r') as f:
+        data = json.load(f)
     return data[key]
+
+
+def load_main_menu_testdata():
+    source = load_test_data(FileNames.LINKS, JSONKeys.MAIN_MENU)
+    return [(item['init_url'], item['partial_selector'], item['expected_url']) for item in source]
