df29e24a16c9b5629ab75cfd7de68a75ae4e6060
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-01 18:12:43 +0200

    Updated

--- test/test_main_menu.py
+++ test/test_main_menu.py
@@ -1,6 +1,11 @@
 import pytest
 from selenium import webdriver
 from pageobject.MainMenu import MainMenu
+from support.TestDataLoader import load_main_menu_testdata
+from support.FileNames import FileNames
+from support.JSONKeys import JSONKeys
+
+testdata = load_main_menu_testdata()
 
 
 @pytest.fixture(scope='function')
@@ -10,14 +15,9 @@ def browser():
     driver.quit()
 
 
-@pytest.mark.parametrize("init_url, partial_selector, expected_url", [
-    ('https://practicesoftwaretesting.com/#/contact', 'nav-home', 'https://practicesoftwaretesting.com/#/'),
-    ('https://practicesoftwaretesting.com/#/', 'nav-contact', 'https://practicesoftwaretesting.com/#/contact'),
-    ('https://practicesoftwaretesting.com/#/', 'nav-sign-in', 'https://practicesoftwaretesting.com/#/auth/login')
-])
+@pytest.mark.parametrize('init_url, partial_selector, expected_url', testdata)
 def test_links(browser, init_url, partial_selector, expected_url):
     main_menu = MainMenu(browser)
     browser.get(init_url)
     main_menu.click_link(partial_selector)
     assert browser.current_url == expected_url, f"Expected URL to be {expected_url}, but got {browser.current_url}"
-
