4c2a8a43252e36b70042329f26797607b5e66df9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 14:23:55 +0200

    Added the Allure step

--- pageobject/MainMenu.py
+++ pageobject/MainMenu.py
@@ -1,3 +1,4 @@
+import allure
 from pageobject.BasePage import BasePage
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -8,5 +9,6 @@ class MainMenu(BasePage):
         super().__init__(driver)
         self._partial_selector = 'a[data-test="'
 
+    @allure.step('Click the link')
     def click_link(self, partial_selector):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self._partial_selector + partial_selector + '"]'))).click()
