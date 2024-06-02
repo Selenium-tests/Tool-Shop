189ec4bd46e62f68385f3bc3e8a292ac6b16e330
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 21:01:03 +0200

    Opening the website added

--- fixtures/Login.py
+++ fixtures/Login.py
@@ -1,9 +1,11 @@
 import pytest
 from fixtures.General import driver
 from pageobject.LoginForm import LoginForm
+from support.URL import URL
 
 
 @pytest.fixture
 def login_form(driver):
+    driver.get(URL.LOGIN_PAGE)
     l_form = LoginForm(driver)
     return l_form
