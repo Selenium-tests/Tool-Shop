f7ee5c5ebcb6f4111b1449a1767ee079b6813659
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 18:16:04 +0200

    Added fields

--- support/JSONKeys.py
+++ support/JSONKeys.py
@@ -1,3 +1,5 @@
 class JSONKeys:
     MAIN_MENU = 'main_menu'
     MAIN_MENU_DROPDOWN_LIST = 'main_menu_dropdown_list'
+    INCORRECT_EMAIL = 'incorrect_email'
+    INCORRECT_PASSWORD = 'incorrect_password'
