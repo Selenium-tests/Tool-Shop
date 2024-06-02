890c634b38a4d7c188e45ad983ea4096186d5fdd
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 10:00:52 +0200

    Added the getter

--- pageobject/BasePage.py
+++ pageobject/BasePage.py
@@ -6,6 +6,9 @@ class BasePage:
         self._driver = driver
         self._wait = WebDriverWait(driver, 10)
 
+    def go_to_page(self, url):
+        self._driver.get(url)
+
     @property
     def driver(self):
         return self._driver
