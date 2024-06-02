7b279d8e4fa54888179dc9569c0c6bae2b252547
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 21:01:53 +0200

    Created the function

--- support/TestDataLoader.py
+++ support/TestDataLoader.py
@@ -17,3 +17,8 @@ def load_main_menu_testdata():
 def load_main_menu_dropdown_list_testdata():
     source = load_test_data(FileNames.LINKS, JSONKeys.MAIN_MENU_DROPDOWN_LIST)
     return [(item['partial_selector'], item['expected_url']) for item in source]
+
+
+def load_credentials_testdata(key):
+    source = load_test_data(FileNames.CREDENTIALS, key)
+    return [(item['email'], item['password']) for item in source]
