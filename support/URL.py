7848cde07cda1b858f5012ff58b28180071c21a1
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-02 18:18:43 +0200

    Added the field

--- support/URL.py
+++ support/URL.py
@@ -1,2 +1,4 @@
 class URL:
     HOME_PAGE = 'https://practicesoftwaretesting.com/#/'
+    LOGIN_PAGE = 'https://practicesoftwaretesting.com/#/auth/login'
+    ACCOUNT_PAGE = 'https://practicesoftwaretesting.com/#/account'
