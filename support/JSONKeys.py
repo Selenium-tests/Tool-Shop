f0739f757c9c186b71cd6a695257ca2bc8e80732
pawelaksman <pablo_xmen@op.pl>
2024-06-03 00:56:05 +0200

    Added the field

--- support/JSONKeys.py
+++ support/JSONKeys.py
@@ -2,4 +2,5 @@ class JSONKeys:
     MAIN_MENU = 'main_menu'
     MAIN_MENU_DROPDOWN_LIST = 'main_menu_dropdown_list'
     INCORRECT_EMAIL = 'incorrect_email'
+    INCORRECT_EMAIL_FORMAT = 'incorrect_email_format'
     INCORRECT_PASSWORD = 'incorrect_password'
