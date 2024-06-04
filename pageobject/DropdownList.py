c3cd6fc3711584081201c5125b602db60cfae6a3
pawelaksman <pablo_xmen@op.pl>
2024-06-05 00:25:52 +0200

    Removed fields

--- pageobject/DropdownList.py
+++ pageobject/DropdownList.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class MainMenuDropdownList(BasePage):
+class DropdownList(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self._partial_selector = 'a[data-test="'
 
-        self._DROPDOWN_LIST_CONTAINER = (By.CSS_SELECTOR, '.dropdown-menu.show')
+        self._DROPDOWN_LIST_CONTAINER = (By.CSS_SELECTOR, 'ul.dropdown-menu.show')
         self._TRIGGER_ELEMENT = (By.CSS_SELECTOR, 'a[data-test="nav-categories"]')
 
     @property
@@ -17,8 +17,8 @@ class MainMenuDropdownList(BasePage):
         return self._DROPDOWN_LIST_CONTAINER
 
     @allure.step('Click the trigger element')
-    def click_trigger_element(self):
-        self.wait.until(EC.element_to_be_clickable(self._TRIGGER_ELEMENT)).click()
+    def click_trigger_element(self, partial_selector):
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, self._partial_selector + partial_selector + '"]'))).click()
 
     @allure.step('Click the link')
     def click_link(self, partial_selector):
