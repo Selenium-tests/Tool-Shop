8f5b0e9c83bdebd69ffb0367ee0ed33c94cc8e0d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-04 08:54:24 +0200

    Added tests

--- pageobject/LoginForm.py
+++ pageobject/LoginForm.py
@@ -8,6 +8,8 @@ class LoginForm(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
+        self._EMAIL_ERROR = (By.ID, 'email-error')
+        self._PASSWORD_ERROR = (By.ID, 'password-error')
         self._LOGIN_ERROR = (By.CSS_SELECTOR, 'div.alert.alert-danger')
 
     locators = {
@@ -31,8 +33,20 @@ class LoginForm(BasePage):
     def click_login_button(self):
         self.wait.until(EC.element_to_be_clickable(self.login_button)).click()
 
-    def login_error_message_locator(self):
-        return self.login_error
+    def email_error_message_locator(self):
+        return self._EMAIL_ERROR
 
-    def login_error_message(self):
+    def email_error_message_content(self):
+        return self.wait.until(EC.visibility_of_element_located(self._EMAIL_ERROR)).text
+
+    def password_error_message_locator(self):
+        return self._PASSWORD_ERROR
+
+    def password_error_message_content(self):
+        return self.wait.until(EC.visibility_of_element_located(self._PASSWORD_ERROR)).text
+
+    def login_error_message_locator(self):
+        return self._LOGIN_ERROR
+
+    def login_error_message_content(self):
         return self.wait.until(EC.visibility_of_element_located(self._LOGIN_ERROR)).text
