95d545a8b914287231adf535af36cc26eaeeed47
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-06-04 06:53:07 +0200

    Updated

--- support/credentials.py
+++ support/credentials.py
@@ -1,6 +1,6 @@
 import os
 
 credentials = {
-    'email': os.getenv('EMAIL'),
-    'password': os.getenv('PASSWORD')
+    'email': os.getenv('EMAIL', 'customer2@practicesoftwaretesting.com'),
+    'password': os.getenv('PASSWORD', 'welcome01')
 }
