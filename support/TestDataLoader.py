2a03e72991646fb6212fd0427b39135b4c423e6a
pawelaksman <pablo_xmen@op.pl>
2024-06-05 23:54:18 +0200

    Added the parameter to the function

--- support/TestDataLoader.py
+++ support/TestDataLoader.py
@@ -14,8 +14,8 @@ def load_main_menu_testdata():
     return [(item['init_url'], item['partial_selector'], item['expected_url']) for item in source]
 
 
-def load_main_menu_dropdown_list_testdata():
-    source = load_test_data(FileNames.LINKS, JSONKeys.MAIN_MENU_DROPDOWN_LIST)
+def load_dropdown_list_testdata(key):
+    source = load_test_data(FileNames.LINKS, key)
     return [(item['item_partial_selector'], item['expected_url']) for item in source]
 
 
