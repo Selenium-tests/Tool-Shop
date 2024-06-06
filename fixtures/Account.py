97bdf7ca2ee23657a9f6efe3a91765b886b38300
pawelaksman <pablo_xmen@op.pl>
2024-06-06 23:12:42 +0200

    Created the fixture

--- fixtures/Account.py
+++ fixtures/Account.py
@@ -1,6 +1,7 @@
 import pytest
 from fixtures.General import driver
 from pageobject.DropdownList import DropdownList
+from pageobject.AccountMenu import AccountMenu
 from support.Authentication import authentication
 
 
@@ -18,3 +19,9 @@ def expanded_account_dropdown_list(driver):
     dropdown_list.click_trigger_element('nav-menu')
     return dropdown_list
 
+
+@pytest.fixture
+def account_menu(driver):
+    authentication(driver)
+    menu = AccountMenu(driver)
+    return menu
